c5505df256f712f567801d4d8dfe7f25647c7e7d
Zhao Kuangshi <contact@zhaokuangshi.cn>
2020-09-23 19:12:37 +0800

    to: solve the pip problem: when required packages are built-in packages, the installation process will be interrupted.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sleepology',
-    version='0.2.21',
+    version='0.2.22',
     author = 'Zhao-Kuangshi',
     author_email = 'contact@zhaokuangshi.cn',
     maintainer = 'Zhao-Kuangshi',
@@ -28,23 +28,11 @@ setup(
     data_files = [('labeltemplate', ['labeltemplate/aasm.labeltemplate'])],
     python_requires = '>=3.5',
     install_requires = ['h5py>=2.10.0',
-                        'tempfile',
                         'numpy>=1.16.4',
                         'matplotlib',
-                        'math',
-                        'os',
-                        'logging',
-                        'trackback',
-                        'traceback',
                         'datetime',
-                        'json',
                         'mne>=0.18.1',
-                        'fractions',
                         'tableprint>=0.9.1',
-                        'random',
                         'sklearn>=0.19.2',
-                        're',
-                        'pylsl',
-                        'itertools',
-                        'reprlib']
+                        'pylsl']
 )
