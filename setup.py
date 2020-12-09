1f17a4031fa08f9a63f0072aa6d31d6d6baa3770
Zhao Kuangshi <contact@zhaokuangshi.cn>
2020-12-09 20:49:18 +0800

    feat: add `array_type` parameter for all of sample-related functions. Make it possible to sample one-hot/N-hot or numeric labels (using `array_type=None`) from the same label dict.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sleepology',
-    version='0.2.36', # TODO: Do not forget changing version!
+    version='0.2.38', # TODO: Do not forget changing version!
     url='https://github.com/Zhao-Kuangshi/sleepology',
     author = 'Zhao-Kuangshi',
     author_email = 'contact@zhaokuangshi.cn',
